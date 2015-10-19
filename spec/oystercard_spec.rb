1dc907a8665c487eb3719c748b0a77c62dc20634
raph <raph@OM.local>
2015-10-19 16:20:42 +0100

    added top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+
   it 'is expected to initialize with a default balance of 0' do
     expect(subject.balance).to eq(0)
   end
 
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+  it 'should top up by any amount' do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
 
 end
